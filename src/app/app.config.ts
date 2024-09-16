b5e9f575aef40460bc65b7e18ffb4c7a252fdb2d
aj-learnings <aayushjalan.learning@gmail.com>
2024-09-16 21:36:18 +0530

    interceptors added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,20 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { QuillModule } from 'ngx-quill';
 import { QuillConfig } from './app.constants';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [ 
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
-    provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
     provideClientHydration(),
     importProvidersFrom(QuillModule.forRoot(
       {
---
+++ src/app/interceptors/auth-interceptor.service.ts
@@ -0,0 +1,33 @@
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor  {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let token: string | null = null;
+
+    if (isPlatformBrowser(this.platformId)) {
+      try {
+        token = localStorage.getItem('authToken');
+      } catch (error) {
+        console.error('Error accessing localStorage:', error);
+      }
+    }
+
+    const requiresAuth = request.url.includes('/api/workbuddy/verify') && request.method !== 'GET';
+    if (requiresAuth && token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+
+    return next.handle(request);
+  }
+}
